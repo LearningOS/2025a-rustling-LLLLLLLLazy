c568c0b18b280831eae1ab90d9fb8d9ac72dfd89
Lazy <lazy@LazydeMacBook-Air.local>
2025-10-20 09:01:25 +0800

    enum

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -46,6 +46,12 @@ impl State {
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
+        match message {
+            Message::ChangeColor(r, g, b) => self.change_color((r, g, b)),
+            Message::Move(point) => self.move_position(point),
+            Message::Echo(s) => self.echo(s),
+            Message::Quit => self.quit(),
+        }
     }
 }
 
